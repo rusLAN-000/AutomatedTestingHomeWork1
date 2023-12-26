a6b6e37db4b05eb3ddb99bc9a5d4c2444697e0e3
rusLAN <puzo00712@gmail.com>
2023-12-26 23:39:20 +0300

    initial commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -6,13 +6,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
     @Test
     void remain() {
-        CashbackHackService service = new CashbackHackService();
         int purchase = 900;
 
         int expected = 100;
+        int actual = service.remain(purchase);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void purchaseEquals1000() {
+        int purchase = 1000;
+
+        int expected = 0;
+        int actual = service.remain(purchase);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void purchaseMore1000() {
+        int purchase = 1200;
+
+        int expected = 800;
         int actual = service.remain(purchase);
         Assertions.assertEquals(expected, actual);
     }
