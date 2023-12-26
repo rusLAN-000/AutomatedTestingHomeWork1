12c6415f4e796eb74b16c80f10b7e35306406bff
rusLAN <puzo00712@gmail.com>
2023-12-26 23:44:39 +0300

    initial commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -5,14 +5,32 @@ import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertEquals;
 
 class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
     @Test
     public void remain() {
-        CashbackHackService service = new CashbackHackService();
         int purchase = 900;
 
         int actual = service.remain(purchase);
         int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void purchaseEquals1000() {
+        int purchase = 1000;
+
+        int expected = 0;
+        int actual = service.remain(purchase);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void purchaseMore1000() {
+        int purchase = 1200;
+
+        int expected = 800;
+        int actual = service.remain(purchase);
         assertEquals(expected, actual);
     }
 }
